05eea88724e3b236f75916fafae351004b50ee6c
Ayon203008 <paypal572874@gmail.com>
2025-12-17 07:54:40 +0600

    new folder structure added

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool, Result } from "pg";
-import path from "path";
-import dotenv from "dotenv";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import config from "./config";
+import initDB, { pool } from "./config/db";
 
 const app = express();
-const port = 3000;
+const port = config.port
 //parser
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STR}`,
-});
-
-const initDB = async () => {
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )`);
-
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )`);
-};
 
 initDB(); // calling the function
 
-app.get("/", (req: Request, res: Response) => {
+
+// middleware logger
+
+const logger =(req:Request,res:Response,next:NextFunction)=>{
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`)
+  next()
+}
+
+app.get("/", logger,(req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
@@ -112,14 +90,16 @@ app.get("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
-
 // update the users data
 app.put("/users/:id", async (req: Request, res: Response) => {
- const {name,email}=req.body
-    try {
-    const result = await pool.query(`
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `
         UPDATE users SET name=$1, email=$2 WHERE id=$3
-        RETURNING *`,[name,email,req.params.id]);
+        RETURNING *`,
+      [name, email, req.params.id]
+    );
 
     console.log(result);
     if (result.rows.length == 0) {
@@ -142,7 +122,7 @@ app.put("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
-
+// delete the data
 app.delete("/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`DELETE FROM users WHERE id=$1`, [
@@ -170,7 +150,57 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+//crud oparetion for todo
 
+//POST todos
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `
+      INSERT INTO todos(user_id,title) VALUES($1,$2) RETURNING *
+      `,
+      [user_id, title]
+    );
+    res.status(201).json({
+      success: true,
+      message: "Todo created successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// get the todos
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      message: "todos get the data successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// NOT found route
+app.use((req:Request,res:Response)=>{
+  res.status(404).json({
+    success:false,
+    message:"Route not found",
+    path:req.path
+
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
